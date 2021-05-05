f7001e220f9eef227df31ea48b53867440a7ae28
Jonjo McKay <jonjo@jonjomckay.com>
2021-05-05 21:44:16 +0100

    Added dark mode theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: ["./public/**/*.html"],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {},
     },
