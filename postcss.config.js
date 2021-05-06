e8b99c9c5b37102db877f6a84158f54e5a25ba97
Jonjo McKay <jonjo@jonjomckay.com>
2021-05-06 21:01:06 +0100

    Removed cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
     plugins: [
         require("autoprefixer"),
         require("tailwindcss"),
-        ...(process.env.JEKYLL_ENV == "production" // example of only using a plugin in production
-            ? [require("cssnano")({ preset: "default" })]
-            : [])
     ]
 };
